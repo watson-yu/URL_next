1be9703a57196fa53fe4183776ca91df08535151
chingchunPro360 <chingchun@pro360.com.tw>
2025-01-13 10:02:24 +0800

    Initial commit from your app

--- src/data/businesses.js
+++ src/data/businesses.js
@@ -4,33 +4,45 @@ import { format } from '../utils/format';
 
 const generateBusinesses = () => {
   const result = {};
+  let globalId = 1;  // 使用全域 ID 計數器確保每個商家有唯一 ID
   
-  Object.entries(locations.countries).forEach(([country, { cities }]) => {
+  // 遍歷 locations 中的每個國家
+  Object.entries(locations.countries).forEach(([country, countryData]) => {
     result[country] = {};
     
-    Object.entries(cities).forEach(([city, districts]) => {
+    // 遍歷每個城市
+    Object.entries(countryData.cities).forEach(([city, districts]) => {
       result[country][city] = {};
       
+      // 遍歷每個區域
       districts.forEach(district => {
         result[country][city][district] = [];
-        let id = 1;
         
-        Object.entries(services.types).forEach(([typeKey, { displayName, services: typeServices }]) => {
-          result[country][city][district].push({
-            id: id++,
-            name: `${district} ${displayName}`,
-            // 儲存格式
-            type: format.toStorageFormat(typeKey),
-            services: typeServices.map(format.toStorageFormat),
-            // 顯示格式
-            displayName,
-            // 位置資訊
-            location: {
-              country,
-              city: format.toStorageFormat(city),
-              district: format.toStorageFormat(district)
-            }
-          });
+        // 為每個區域生成所有類型的商家
+        Object.entries(services.types).forEach(([typeKey, typeInfo]) => {
+          // 為每個類型生成 2-3 個商家
+          const numBusinesses = Math.floor(Math.random() * 2) + 2; // 2-3 個商家
+          
+          for (let i = 0; i < numBusinesses; i++) {
+            const businessName = `${district} ${typeInfo.displayName} ${i + 1}`;
+            
+            result[country][city][district].push({
+              id: globalId++,
+              name: businessName,
+              // 儲存格式
+              type: format.toStorageFormat(typeKey),
+              // 所有可用的服務
+              services: typeInfo.services.map(format.toStorageFormat),
+              // 顯示名稱
+              displayName: typeInfo.displayName,
+              // 位置資訊
+              location: {
+                country,
+                city: format.toStorageFormat(city),
+                district: format.toStorageFormat(district)
+              }
+            });
+          }
         });
       });
     });
