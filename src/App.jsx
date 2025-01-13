77d8118530beb72719d3c39061585ad85fe4e983
chingchunPro360 <chingchun@pro360.com.tw>
2025-01-13 17:07:22 +0800

    Initial commit from your app

--- src/App.jsx
+++ src/App.jsx
@@ -14,17 +14,12 @@ export default function App() {
     <MantineProvider withGlobalStyles withNormalizeCSS>
       <Router>
         <Routes>
-          {/* 基本路由 */}
           <Route path="/" element={<HomePage />} />
           <Route path="/home/:type" element={<TypePage />} />
-          
-          {/* 服務相關路由 - 需要在一般城市/區域路由之前 */}
-          <Route path="/home/:type/:service-:city" element={<ServiceCityPage />} />
-          <Route path="/home/:type/:service-:city/:district" element={<ServiceDistrictPage />} />
-          
-          {/* 一般城市/區域路由 */}
           <Route path="/home/:type/:city" element={<CityPage />} />
           <Route path="/home/:type/:city/:district" element={<DistrictPage />} />
+          <Route path="/home/:type/:service-:city" element={<ServiceCityPage />} />
+          <Route path="/home/:type/:service-:city/:district" element={<ServiceDistrictPage />} />
           <Route path="/home/:type/:city/:district/:businessId" element={<BusinessDetail />} />
         </Routes>
       </Router>
--- src/pages/DistrictPage.jsx
+++ src/pages/DistrictPage.jsx
@@ -18,5 +18,142 @@ export default function DistrictPage() {
   const businessList = businesses.getByTypeAndCityAndDistrict(type, city, district);
   console.log('Businesses found:', businessList?.length); // Debug
 
-  // ... rest of the component
+  // 如果類型不存在，顯示錯誤頁面
+  if (!typeInfo) {
+    return (
+      <Container size="md" py="xl">
+        <Title order={1} align="center" mb="xl">
+          Service Type Not Found
+        </Title>
+        <Text align="center" mb="xl">
+          The service type does not exist in our directory.
+        </Text>
+        <Group position="center">
+          <Button onClick={() => navigate('/')}>
+            Back to Home
+          </Button>
+        </Group>
+      </Container>
+    );
+  }
+
+  // 如果城市或區域不存在，顯示錯誤頁面
+  if (!locationUtils.isCityValid(city) || !locationUtils.isDistrictValid(city, district)) {
+    return (
+      <Container size="md" py="xl">
+        <Title order={1} align="center" mb="xl">
+          Location Not Found
+        </Title>
+        <Text align="center" mb="xl">
+          The specified location does not exist in our directory.
+        </Text>
+        <Group position="center">
+          <Button onClick={() => navigate(generatePath.type(type))}>
+            Back to {typeInfo.displayName}
+          </Button>
+          <Button onClick={() => navigate('/')}>
+            Back to Home
+          </Button>
+        </Group>
+      </Container>
+    );
+  }
+
+  const breadcrumbItems = [
+    {
+      label: typeInfo.displayName,
+      path: generatePath.type(type)
+    },
+    {
+      label: format.toDisplay(city),
+      path: generatePath.city(type, city)
+    },
+    {
+      label: format.toDisplay(district),
+      path: generatePath.district(type, city, district)
+    }
+  ];
+
+  // 服務按鈕
+  const renderServiceButtons = () => (
+    <Box mb="xl">
+      <Title order={3} size="h4" mb="md">Available Services</Title>
+      <Group spacing="sm" noWrap sx={{ padding: '4px', overflowX: 'auto' }}>
+        {typeInfo.services.map((service) => (
+          <Button
+            key={service}
+            variant="light"
+            color={typeInfo.color}
+            onClick={() => navigate(generatePath.serviceDistrict(type, service, city, district))}
+            sx={{ flexShrink: 0 }}
+          >
+            {format.toDisplay(service)}
+          </Button>
+        ))}
+      </Group>
+    </Box>
+  );
+
+  // 其他區域按鈕
+  const renderOtherDistrictButtons = () => {
+    const otherDistricts = locationUtils.getDistrictsForCity(city)
+      .filter(d => d !== district);
+
+    return (
+      <Box mb="xl">
+        <Title order={3} size="h4" mb="md">Other Districts in {format.toDisplay(city)}</Title>
+        <Group spacing="sm" noWrap sx={{ padding: '4px', overflowX: 'auto' }}>
+          {otherDistricts.map((d) => (
+            <Button
+              key={d}
+              variant="light"
+              onClick={() => navigate(generatePath.district(type, city, d))}
+              sx={{ flexShrink: 0 }}
+            >
+              {`${typeInfo.displayName} in ${format.toDisplay(d)}`}
+            </Button>
+          ))}
+        </Group>
+      </Box>
+    );
+  };
+
+  // 其他商家類別按鈕
+  const renderOtherTypeButtons = () => {
+    const otherTypes = Object.entries(services.types)
+      .filter(([t]) => t !== type);
+
+    return (
+      <Box mb="xl">
+        <Title order={3} size="h4" mb="md">Other Business Types</Title>
+        <Group spacing="sm" noWrap sx={{ padding: '4px', overflowX: 'auto' }}>
+          {otherTypes.map(([t, info]) => (
+            <Button
+              key={t}
+              variant="light"
+              onClick={() => navigate(generatePath.district(t, city, district))}
+              sx={{ flexShrink: 0 }}
+            >
+              {info.displayName}
+            </Button>
+          ))}
+        </Group>
+      </Box>
+    );
+  };
+
+  return (
+    <Container size="md" py="xl">
+      <Breadcrumbs items={breadcrumbItems} />
+      <Title order={1} align="center" mb="md">
+        {typeInfo.displayName} in {format.toDisplay(district)}, {format.toDisplay(city)}
+      </Title>
+
+      <SearchBar />
+      {renderServiceButtons()}
+      {renderOtherDistrictButtons()}
+      {renderOtherTypeButtons()}
+      <BusinessGrid businesses={businessList} />
+    </Container>
+  );
 }
